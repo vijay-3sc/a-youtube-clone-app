0b5a58c18d9ffb7604bf2d557201b6b239fc6aba
vijay_3sc <kanugula.vijay@3scsolution.com>
2024-07-26 12:46:00 +0530

    this is the new file

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -9,7 +9,7 @@ pipeline{
     stages{
         stage('git checkout'){
             steps{
-                checkout scmGit(branches:[[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/vijay-3sc/a-youtube-clone-app']])
+                checkout scmGit(branches:[[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/vijay-3sc/a-youtube-clone-app']]){
             }
         }
         stage('build docker image'){
@@ -20,13 +20,25 @@ pipeline{
         }
         stage('push image'){
             steps{
-                withCredentials([usernamepassword(credentialsId: 'acr_creds', passwordVariable: 'password', usernameVariable:'username')]){
                 sh 'docker login -u ${username} -p ${password} ${ACR_LOGIN_SERVER}'
                 sh 'docker push ${ACR_LOGIN_SERVER}/${REPO_NAME}:$BUILD_NUMBER'
 
                 }
                 
             }
-        }
+        stage('deploy web app'){
+            steps{
+                withCredentials([azureServicePrincipal('AZURE_SERVICE_PRINCIPAL')]) {
+                sh 'az login --service-principal -u ${AZURE_CLIENT_ID} -p ${AZURE_CLIENT_SECRET} --tenant ${AZURE_TENANT_ID}'
+                }
+                withCredentials([usernamePassword(credentialsId: 'acrprincipal', passwordVariable: 'password', usernameVariable: 'username')]) {
+                sh 'az webapp config container set --name ${REGISTRY_NAME} --resource-group payment --docker-custom-image-name ${ACR_LOGIN_SERVER}/${REPO_NAME}:$BUILD_NUMBER --docker-registry-server-url https://vijay.azurecr.io --docker-registry-server-user ${username} --docker-registry-server-password ${password}'
+                }
+            }
+            
+        }   
+        
     }
+
+}
 }
